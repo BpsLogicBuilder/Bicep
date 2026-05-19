9db4186a1e66d863ea6934b32f493d33a2f452ad
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-05-19 14:51:45 -0400

    CosmosDb for NoSQL (#11)

---
+++ CosmosDbNoSqlClient/CosmosDbNoSqlClient/Chunk.cs
@@ -0,0 +1,9 @@
+﻿namespace CosmosDbNoSqlClient;
+internal class Chunk
+{
+    public string documentId { get; set; } = "";
+    public string id { get; set; } = "";
+    public string Content { get; set; } = "";
+    public Metadata? Metadata { get; set; }
+    public string uniqueId { get; set; } = "";
+}
