d95c82283c41cfbf18505afb572eefb56ce128fe
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-05-28 16:54:51 -0400

    Workflow and bicep files for key vault. (#18)
    * Workflow and Bicep files for key vault deployment.
    
    * Key vault console app.

---
+++ KeyVaultConsole/KeyVaultConsole/NewSecretVersionCreator.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Threading.Tasks;
+
+namespace KeyVaultConsole
+{
+    internal static class NewSecretVersionCreator
+    {
+        internal static async Task CreateNewVersion(SecretClient secretClient, string secretName, string newValue)
+        {
+			try
+			{
+				var secret = (await secretClient.GetSecretAsync(secretName)).Value;
+                Console.WriteLine($"Old value was: {secret.Value}");
+            }
+			catch (Exception ex)
+			{
+                Console.WriteLine($"Error getting existing secret: {ex}");
+            }
+
+            var newSecret = new KeyVaultSecret(secretName, newValue);
+            newSecret.Properties.ContentType = "text/plain";
+            newSecret.Properties.Tags.Add("environment", "development");
+            newSecret.Properties.Tags.Add("rotated", "true");
+            await secretClient.SetSecretAsync(newSecret);
+
+            Console.WriteLine($"Successfully created new version of the secret.");
+        }
+    }
+}
---
+++ KeyVaultConsole/KeyVaultConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using dotenv.net;
+using KeyVaultConsole;
+using System;
+
+DotEnv.Load();
+var envVars = DotEnv.Read();
+string ENDPOINT = envVars["ENDPOINT"];
+
+DefaultAzureCredentialOptions options = new()
+{
+    ExcludeEnvironmentCredential = true,
+    ExcludeManagedIdentityCredential = true
+};
+
+SecretClient client = new(new Uri(ENDPOINT), new DefaultAzureCredential(options));
+
+string? input = "";
+while (input != "4")
+{
+    Console.Clear();
+    Console.WriteLine("=== Menu ===");
+    Console.WriteLine("1. Retrive Secrets");
+    Console.WriteLine("2. Retrive Secret Properties");
+    Console.WriteLine("3. Create New Secret Version");
+    Console.WriteLine("4. Quit");
+    Console.Write("Enter your choice: ");
+
+    input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            await SecretsRetriever.Retrieve(client);
+            break;
+        case "2":
+            await SecretPropertiesLister.ListProperties(client);
+            break;
+        case "3":
+            await NewSecretVersionCreator.CreateNewVersion(client, "openai-api-key", Guid.NewGuid().ToString());
+            break;
+        case "4":
+            Console.WriteLine("Exiting application...");
+            break;
+        default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+    }
+
+    if (input != "4")
+    {
+        Console.WriteLine("\nPress any key to continue...");
+        Console.ReadKey();
+    }
+}
